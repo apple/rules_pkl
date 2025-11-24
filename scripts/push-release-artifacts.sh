0af7ba9341f08d1c9464cb12d03bc42962cf1b22
Romain Chossart <rchossart@apple.com>
2025-11-24 17:18:01 +0000

    build: specify repo in push-release-artifacts script

--- scripts/push-release-artifacts.sh
+++ scripts/push-release-artifacts.sh
@@ -24,4 +24,4 @@ ARCHIVE_NAME="rules_pkl-${VERSION}.tar.gz"
 
 # Note: we use `.gitattributes` to filter things from the release archive
 git archive --format=tar --prefix=rules_pkl-${VERSION}/ "v${VERSION}" | gzip > "${ARCHIVE_NAME}"
-gh release upload "v${VERSION}" "${ARCHIVE_NAME}" --clobber
+gh release upload --repo github.com/apple/rules_pkl "v${VERSION}" "${ARCHIVE_NAME}" --clobber
